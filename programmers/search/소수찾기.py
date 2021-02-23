156fc5eb67cedfe5c491e795ff40d6cd37e019ff
chulhee23 <chulhee2351@gmail.com>
2021-02-23 12:03:32 +0900

    완전탐색 소수찾기 lv2

---
+++ programmers/search/소수찾기.py
@@ -0,0 +1,31 @@
+import itertools
+
+def prime(number):
+    if number != 1:
+        for i in range(2, number):
+            if number % i == 0:
+                return False
+        return True
+    return False
+
+
+def solution(numbers):
+    answer = 0 
+
+    arr = [i for i in numbers]
+    primes = []
+    for i in range(1, len(arr)+1):
+        p = itertools.permutations(arr, i) # 튜플로 반환
+        for pp in p:
+            result = int(''.join(pp))
+            if result != 0:
+                if prime(result):
+                    primes.append(result)
+
+    return len(list(set(primes)))
+
+
+
+print(solution("17"))
+
+print(solution("011"))
