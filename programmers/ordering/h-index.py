5c2b06edacf947ecd1f681b25b600a39d397c832
chulhee23 <chulhee2351@gmail.com>
2021-02-27 11:03:26 +0900

    h-index

--- programmers/ordering/h-index.py
+++ programmers/ordering/h-index.py
@@ -3,13 +3,17 @@ def solution(citations):
 
     h = len(citations)
 
-    while h >= 0:
+    while h > 0:
         if (citations[h-1] >= h) and h == len(citations):
             return h
 
-        if (citations[h-1] >= h) and (citations[h] < h):
+        if (citations[h-1] >= h) and (citations[h] <= h):
             return h
 
         h = h - 1
 
     return h
+
+
+print(solution([2, 0, 6, 1, 5]))
+print(solution([0,0,0,0,0,0]))
