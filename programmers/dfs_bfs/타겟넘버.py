6ab1db7834cd966aa03d94b565aa832998f2a356
chulhee23 <chulhee2351@gmail.com>
2021-03-06 09:00:57 +0900

    타겟넘버

--- programmers/dfs_bfs/타겟넘버.py
+++ programmers/dfs_bfs/타겟넘버.py
@@ -1,14 +1,39 @@
-from collections import deque
+# from collections import deque
+
+
+# def dfs(numbers, target, idx, s):
+#     global answer
+#     N = len(numbers)
+#     if(idx == N and target == s):
+#         answer += 1
+#         return
+#     if(idx == N):
+#         return
+#     dfs(numbers, target, idx + 1,  s + numbers[idx])
+#     dfs(numbers, target, idx + 1, s - numbers[idx])
+
+# def solution(numbers, target):
+#     global answer
+#     answer = 0
+#     idx = 0
+#     s = 0
+#     dfs(numbers, target, idx ,s)    
+    
+#     return answer
+
+from itertools import product
+
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    print(l)
+    s = list(map(sum, product(*l)))
+    print(product(*l))
+    print(s)
+    return s.count(target)
+
+
 
 numbers = [1, 1, 1, 1, 1]
 target = 3
 
-def solution(numbers, target):
-    answer = 0
-    
-    return answer
-
-
-
 print(solution(numbers, target))
-# 3
