4fe9feceb92c59731d6ce9ebc0684a281e84b12e
chulhee23 <chulhee2351@gmail.com>
2021-03-08 21:52:44 +0900

    bfs solution 추가

--- programmers/dfs_bfs/네트워크.py
+++ programmers/dfs_bfs/네트워크.py
@@ -1,3 +1,32 @@
+from collections import deque
+
+
+def bfs_sol(n, computers):
+    answer = 0
+
+    visited = [False for _ in range(n)]
+    queue = deque()
+    for node in range(n):
+        if visited[node] == False:  # node 가 방문한 적이 없으면
+            queue.append(node)  # queue에 추가한 뒤, 확인 들어가자.
+            while queue:
+                cur_node = queue.pop()
+                visited[cur_node] = True  # 방문 처리
+                for tmp_node in range(n):
+                    #  현재 노드와 상대 노드가 연결 처리된 경우
+                    #  상대 노드를 확인해봐야함. -> queue에 추가해서 돌리자.
+                    if cur_node != tmp_node and computers[cur_node][tmp_node] == 1:  
+                        if visited[tmp_node] == False:
+                            queue.append(tmp_node)
+
+            answer += 1
+        else:
+            # 방문한 적이 있다?
+            # 이전 노드에서 방문처리가 되었으므로 연결이 되어 있음
+            continue
+
+    return answer
+
 
 def solution(n, computers):
     answer = 0
@@ -11,7 +40,9 @@ def solution(n, computers):
             answer += 1
 
     return answer
-    
+
+
+# dfs 에서 하는 일을 바라보자
 def dfs(n, computers, node, visited):
     visited[node] = True
     for connect in range(n):
@@ -22,6 +53,9 @@ def dfs(n, computers, node, visited):
                 dfs(n, computers, connect, visited)
 
 
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
 print("------")
+print(bfs_sol(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+print(bfs_sol(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
+print("------")
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
 print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
