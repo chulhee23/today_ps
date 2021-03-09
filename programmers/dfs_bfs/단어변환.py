2288f2b0cda30f83bd0b403fa840276915947b6f
chulhee23 <chulhee2351@gmail.com>
2021-03-10 00:07:02 +0900

    단어변환 fail

--- programmers/dfs_bfs/단어변환.py
+++ programmers/dfs_bfs/단어변환.py
@@ -1,4 +1,16 @@
+from collections import deque
 def solution(begin, target, words):
-
     answer = 0
+
+
+    queue = deque()
+
+    queue.append(begin)
+    
+    while queue:
+        word = queue.popleft()
+
+        
+
+
     return answer
