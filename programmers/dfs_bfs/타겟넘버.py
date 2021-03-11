f689ad8ec6f53ca9a5704ed8adeb3ac58d4b0a33
chulhee23 <chulhee2351@gmail.com>
2021-03-11 17:40:23 +0900

    타겟넘버 bfs 풀이

--- programmers/dfs_bfs/타겟넘버.py
+++ programmers/dfs_bfs/타겟넘버.py
@@ -21,19 +21,66 @@
     
 #     return answer
 
-from itertools import product
+# from itertools import product
 
-def solution(numbers, target):
-    l = [(x, -x) for x in numbers]
-    print(l)
-    s = list(map(sum, product(*l)))
-    print(product(*l))
-    print(s)
-    return s.count(target)
+# def solution(numbers, target):
+#     l = [(x, -x) for x in numbers]
+#     print(l)
+#     s = list(map(sum, product(*l)))
+#     print(product(*l))
+#     print(s)
+#     return s.count(target)
 
+
+
+# numbers = [1, 1, 1, 1, 1]
+# target = 3
+
+# print(solution(numbers, target))
+
+
+
+
+
+# 복습---------
+from collections import deque
+
+# bfs 풀이
+
+def solution2(numbers, target):
+    queue = deque([0])
+
+    count = 1
+
+    for number in numbers:
+        tmp = count
+        count *= 2
+        while queue and tmp > 0:
+            item = queue.popleft()
+
+            queue.append(item + number)
+            queue.append(item - number)
+            tmp -= 1
+
+
+    print(list(queue))
+    answer = len(list(filter(lambda x: x == target, list(queue))))
+    return answer
 
 
 numbers = [1, 1, 1, 1, 1]
 target = 3
 
-print(solution(numbers, target))
+print(solution2(numbers, target))
+
+
+
+
+
+
+
+
+
+
+
+
