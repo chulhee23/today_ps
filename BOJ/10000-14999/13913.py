37d52d94f4e8ecc90e6be19d9785cedb2b9f1bb8
chulhee23 <chulhee2351@gmail.com>
2021-03-25 15:51:16 +0900

    [#4] 13913 숨바꼭질4 | 메모리 초과 해결 -> 연결리스트로 메모리 초과 회피

--- BOJ/10000-14999/13913.py
+++ BOJ/10000-14999/13913.py
@@ -3,21 +3,30 @@ from collections import deque
 n, k = map(int, input().split())
 
 queue = deque()
+queue.append((n, 0))
 
-queue.append((n, 0, [n]))
+visited = [-1] * 100001
 
 while queue:
-    cur, count, visited = queue.popleft()
+    cur, count = queue.popleft()
     if cur == k:
         print(count)
-        print(*visited, sep=" ")
         break
-    if cur + 1 < 100000:
-        
-        queue.append((cur + 1, count + 1, visited + [cur + 1]))
-    if cur - 1 >= 0:
-        queue.append((cur - 1, count + 1, visited + [cur - 1]))
-    if cur * 2 < 100000:
-        queue.append((cur * 2, count + 1, visited + [cur * 2]))
 
+    if cur + 1 < 100001 and visited[cur+1] == -1:
+        queue.append((cur + 1, count + 1))
+        visited[cur + 1] = cur
+    if cur - 1 >= 0 and visited[cur-1] == -1:
+        queue.append((cur - 1, count + 1))
+        visited[cur - 1] = cur
+    if cur * 2 < 100001 and visited[cur * 2] == -1:
+        queue.append((cur * 2, count + 1))
+        visited[cur * 2] = cur
 
+path = []
+for _ in range(count + 1):
+    path.append(cur)
+    bef = visited[cur]
+    cur = bef
+
+print(*path[::-1])
