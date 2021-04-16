50b82088cd46e2e626ae7b2bf85e844641fa8130
chulhee23 <chulhee2351@gmail.com>
2021-04-16 15:59:12 +0900

    [#7] 1890. DP를 BFS로  오해한 문제. 근거 확실하게 잡아야 한다

---
+++ BOJ/01000-04999/1890.py
@@ -0,0 +1,49 @@
+import sys
+input = sys.stdin.readline
+
+
+
+
+
+
+
+# BFS로 잘못된 풀이
+'''
+잘못된 근거
+- 문제에 힌트가 있다.
+전체 경로의 수 2^63-1개가 나올 수 있는데, 이를 하나씩 헤는 것은 무리가 있다.
+당연하게 여겨야 한다.
+또한 엣지 케이스에 대해 항상 주의하면서 짜자.
+'''
+# from collections import deque
+
+# n = int(input())
+# arr = [ list(map(int, input().split())) for _ in range(n)]
+
+# queue = deque()
+# queue.append((0, 0))
+
+# dx = [1, 0]
+# dy = [0, 1]
+
+# ans = 0
+
+# while queue:
+#     x, y = queue.popleft()
+#     weight = arr[x][y]
+#     for i in range(2):
+#         if arr[x][y] == 0:
+#             continue
+
+#         nx = x + dx[i] * weight
+#         ny = y + dy[i] * weight
+
+#         if nx == n-1 and ny == n - 1:
+#             ans += 1
+#             continue
+        
+#         if 0 <= nx < n and 0 <= ny < n:
+#             if arr[nx][ny] != 0:
+#                 queue.append((nx, ny))
+
+# print(ans)
