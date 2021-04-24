6514606cf275a7ed4ac371f4b14c28c9be003829
chulhee23 <chulhee2351@gmail.com>
2021-04-24 12:06:40 +0900

    [프로그래머스] bfs 단어변환

--- programmers/dfs_bfs/단어변환.py
+++ programmers/dfs_bfs/단어변환.py
@@ -1,8 +1,30 @@
 from collections import deque
 
 
-def solution(begin, target, words):
-    if target not in words:
-        return 0
+def diff(word, other):
+    count = 0
+    for i in range(len(word)):
+        if word[i] != other[i]:
+            count += 1
+    return count
 
-    
+
+def solution(begin, target, words):
+
+    visited = [False] * len(words)
+    queue = deque()
+    queue.append((begin, 0))
+    ans = 0
+    while queue:
+        word, count = queue.popleft()
+
+        if word == target:
+            return count
+
+        for i in range(len(words)):
+            nx = words[i]
+            if diff(word, nx) == 1 and visited[i] == False:
+                visited[i] = True
+                queue.append((nx, count + 1))
+
+    return ans
