5cd61111f5a1f7a3c1f8e31a8d1bd6eb11b8e1f4
chulhee23 <chulhee2351@gmail.com>
2021-05-04 20:42:42 +0900

    백트래킹 스타트와 링크

--- BOJ/10000-14999/14889.py
+++ BOJ/10000-14999/14889.py
@@ -1,45 +1,33 @@
-import itertools
-import sys
-
-input = sys.stdin.readline
-
+# 백트래킹
+from itertools import combinations, permutations
 
 n = int(input())
 
-arr = [ list(map(int , input().split())) for _ in range(n)]
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 
-members = [i for i in range(n)]
-start_teams = itertools.combinations(members, n//2)
-# link 구하기 위해서 set 자료형 사용!
-members = set(members)
+members = list(range(0, n))
 
-ans = sys.maxsize
-for start_team in start_teams:
-    start = set(list(start_team))
-    start_team_pairs = list(itertools.combinations(start, 2))
+start = combinations(members, n // 2)
 
-    link = list(members - start)
-    link_team_pairs = list(itertools.combinations(link, 2))
+ans = float('inf')
+
+for team1 in start:
+    team2 = list(set(members) - set(team1))
+    team1 = list(team1)
+
+    tmps = permutations(team1, 2)
+    sum1 = 0
+    for tmp in tmps:
+        i = tmp[0]
+        j = tmp[1]
+        sum1 += arr[i][j]
     
-    start_sum = 0
-    link_sum = 0
-    
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                continue
-            if j > i:
-                if (i, j) in start_team_pairs:
-                    start_sum += arr[i][j] + arr[j][i]
-                    
-                if (i, j) in link_team_pairs:
-                    link_sum += arr[i][j] + arr[j][i]
-    
-    ans = min(abs(start_sum - link_sum), ans)
-    
-
-
+    tmps = permutations(team2, 2)
+    sum2 = 0
+    for tmp in tmps:
+        i = tmp[0]
+        j = tmp[1]
+        sum2 += arr[i][j]
+    ans = min(abs(sum1 - sum2), ans)
 print(ans)
-
-
