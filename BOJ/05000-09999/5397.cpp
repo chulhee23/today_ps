1d99bacb38da0a4e86a83100b85f2e4801ec327a
chulhee23 <chulhee2351@gmail.com>
2021-07-18 02:19:55 +0900

    5397 복습 필요gs

---
+++ BOJ/05000-09999/5397.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+#include <list>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int test_case;
+  cin >> test_case;
+
+  for(int i = 0; i < test_case; i++){
+    string str;
+    cin >> str;
+    list<char> ans;
+    auto cur = ans.begin(); // cursor 의 초기 위치
+
+    for(int i = 0; i < str.length(); i++){
+      if(str[i] == '<'){
+        if(cur == ans.begin()) continue;
+        cur--;
+      }
+      else if(str[i] == '>'){
+        if(cur == ans.end()) continue;
+        cur++;
+      }
+      else if(str[i] == '-'){
+        if(cur == ans.begin()) continue;
+        ans.erase(--cur); // 삭제한 원소의 다음 원소를 가리킨다 
+        // 이해 안감... 다시 복습
+      }
+      else {
+        ans.insert(cur, str[i]);
+      }
+
+    }
+
+    for(auto it = ans.begin(); it != ans.end(); it++){
+      cout << *it;
+    }
+    cout << '\n';
+
+
+  }
+  return 0;
+}
