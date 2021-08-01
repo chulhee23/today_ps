acffa3fb4b318de71a3797ea2223562cf1cf4156
chulhee23 <chulhee2351@gmail.com>
2021-08-01 22:08:32 +0900

    swea

---
+++ SWEA/1768/solution.cpp
@@ -0,0 +1,26 @@
+#define N 4
+
+typedef struct
+{
+
+  int strike;
+
+  int ball;
+
+} Result;
+
+// API
+
+extern Result query(int guess[]);
+
+void doUserImplementation(int guess[])
+{
+
+  // Implement a user's implementation function
+
+  //
+
+  // The array of guess[] is a return array that
+
+  // is your guess for what digits[] would be.
+}
