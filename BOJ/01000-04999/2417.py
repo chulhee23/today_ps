2d7a6ba4204e49ac20284c605084b36c36db3a4e
chulhee23 <chulhee2351@gmail.com>
2021-08-26 20:35:48 +0900

    2417

---
+++ BOJ/01000-04999/2417.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+left = 0
+right = n
+while left <= right:
+    mid = (left + right) // 2
+    if mid ** 2 < n:
+        left = mid + 1
+    else:
+        right = mid - 1
+        
+
+print(left)
