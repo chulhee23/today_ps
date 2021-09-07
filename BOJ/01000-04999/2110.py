5d02b414809358336226a1717a454f35e9c34c4c
chulhee23 <chulhee2351@gmail.com>
2021-09-07 19:28:10 +0900

    /

---
+++ BOJ/01000-04999/2110.py
@@ -0,0 +1,11 @@
+# fail
+import sys
+input = sys.stdin.readline
+
+n, c = map(int, input().split())
+
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+
---
+++ BOJ/01000-04999/4256.py
@@ -0,0 +1,38 @@
+# fail
+import sys
+input = sys.stdin.readline
+
+
+class Node:
+    def __init__(self, value, left = None, right = None):
+        v = value
+    
+    def append_left(node):
+        left = node
+        return
+    def append_left(node):
+        right = node
+        return
+
+    def postorder(self):
+        if self.left != None:
+            self.postorder(self.left)
+        if self.right != None:
+            self.postorder(self.right)
+        print(self.v)
+
+inorder, preorder = [], []
+
+
+def init(n):
+    preorder = [map(int, input().split())]
+    inorder = [map(int, input().split())]
+    
+
+    return
+
+
+t = int(input())
+for test in range(t):
+    n = int(input())
+    init(n)
